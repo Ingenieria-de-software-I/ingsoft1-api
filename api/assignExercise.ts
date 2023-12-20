2af38efcca7b7c5bb38123b8210f62a578fd9993
Borja Garibotti <mgaribotti@fi.uba.ar>
2023-12-20 16:39:22 -0300

    Agregar endpoints para Notion

---
+++ api/assignExercise.ts
@@ -0,0 +1,8 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+
+import { api } from '../app/production/api';
+
+export default async function (req: VercelRequest, res: VercelResponse) {
+    const response = await api.assignExerciseHandler({ ...req.body });
+    res.status(response.code).send(response.message);
+}
